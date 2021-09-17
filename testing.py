458804bd9280360582f35ef061c081b0028f620e
Alberto <alberl13@ucm.es>
2021-09-17 16:04:18 +0200

    Size issues

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
+from posixpath import sep
 import subprocess
+import math
 
 
 def find(name, path):
@@ -8,15 +10,11 @@ def find(name, path):
         if name in files:
             return os.path.join(root, name)
 
-def extract_folder_name(path):
-    last_pos = len(path)-1
-    while path[last_pos] != os.sep():
-        last_pos = last_pos-1
-    folder = ""
-    while last_pos != len(path)-1:
-        folder = folder + path[last_pos]
-        last_pos += 1
-    return folder
+def get_folder_size(path):
+    size = 0
+    for ele in os.scandir(path):
+        size+=os.path.getsize(ele)
+    return size
 
 def user_input():
     if os.path.isdir("C:/Program Files/VeraCrypt") and os.path.isfile("C:/Program Files/VeraCrypt/VeraCrypt Format.exe"):       
@@ -24,15 +22,24 @@ def user_input():
         folder = input("Enter the folder to encrypt: ")
         folderPath = Path(folder)
         os.chdir(folderPath)
-        foldername = os.path.basename(folderPath)
-        print(foldername)
+        cmd_foldername = os.path.basename(folderPath)
+        cmd_size = os.path
+        cmd_volumepath = folder+os.sep+cmd_foldername+".hc"
+        aux_size = get_folder_size(folder) 
+        size = (1.25 * aux_size)
+        if size >= 1024:
+            cmd_size = "10M"
+        else:
+            cmd_size = repr(math.ceil(size/1024))+"K"
+
+        print(cmd_size)
         base = "C:"+os.sep
         str = find("VeraCrypt.exe", base)
         newPath = str.replace(os.sep, '/')
         pathObject = Path(newPath)
         VCpath = pathObject.parent.absolute()
         os.chdir(VCpath)
-        subprocess.call(["VeraCrypt Format.exe","/create", "C:/Users/alber/Desktop/pistacho.hc","/password", "test", "/hash", "sha512", "/encryption", "serpent", "/filesystem", "FAT", "/size", "10M", "/force"])
+        subprocess.call(["VeraCrypt Format.exe","/create", cmd_volumepath,"/password", "test", "/hash", "sha512", "/encryption", "serpent", "/filesystem", "FAT", "/size", cmd_size, "/force"])
     # "C:\Program Files\VeraCrypt\VeraCrypt Format.exe" /create c:\Data\test.hc "/password test /hash sha512 /encryption serpent" /filesystem FAT /size 10M /force
     else:
         print("Veracrypt not in system")
