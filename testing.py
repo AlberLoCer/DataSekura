96edb675edff784a23f7ccdcca00486a1dd0f62e
Alberto <alberl13@ucm.es>
2021-09-17 13:07:35 +0200

    First steps on user input for encrypting

--- testing.py
+++ testing.py
@@ -9,12 +9,17 @@ def find(name, path):
             return os.path.join(root, name)
 
 
-            
-base = "C:"+os.sep
-str = find("VeraCrypt.exe", base)
-newPath = str.replace(os.sep, '/')
-pathObject = Path(newPath)
-VCpath = pathObject.parent.absolute()
-os.chdir(VCpath)
+if os.path.isdir("C:/Program Files/VeraCrypt") and os.path.isfile("C:/Program Files/VeraCrypt/VeraCrypt Format.exe"):       
+    print("VeraCrypt is installed!") 
+    folder = input("Enter the folder to encrypt: ")
+    folderPath = str(folder)
+    base = "C:"+os.sep
+    str = find("VeraCrypt.exe", base)
+    newPath = str.replace(os.sep, '/')
+    pathObject = Path(newPath)
+    VCpath = pathObject.parent.absolute()
+    os.chdir(VCpath)
+    subprocess.call(["VeraCrypt Format.exe","/create", "C:/Users/alber/Desktop/pistacho.hc","/password", "test", "/hash", "sha512", "/encryption", "serpent", "/filesystem", "FAT", "/size", "10M", "/force"])
 # "C:\Program Files\VeraCrypt\VeraCrypt Format.exe" /create c:\Data\test.hc "/password test /hash sha512 /encryption serpent" /filesystem FAT /size 10M /force
-subprocess.call(["VeraCrypt Format.exe","/create", "C:/Users/alber/Desktop/pistacho.hc","/password", "test", "/hash", "sha512", "/encryption", "serpent", "/filesystem", "FAT", "/size", "10M", "/force"])
+else:
+    print("Veracrypt not in system")
