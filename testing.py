365767dd0e2dad3682d9c343d7f087c7b3367a61
Alberto <alberl13@ucm.es>
2021-09-18 16:31:02 +0200

    Menu added + bit of re-organization

--- testing.py
+++ testing.py
@@ -16,24 +16,51 @@ def get_folder_size(path):
         size+=os.path.getsize(ele)
     return size
 
+def check_VC_integrity():
+    return os.path.isdir("C:/Program Files/VeraCrypt") and os.path.isfile("C:/Program Files/VeraCrypt/VeraCrypt Format.exe")
+
+def print_encryption_menu():
+    print("Choose an encryption algorithm: \n")
+    print("1. AES")
+    print("2. Serpent")
+    print("3. Twofish")
+    print("4. Camellia")
+    print("5. Kuznyechik")
+
+def choose_encryption(input):
+    switcher = {
+        1: "aes",
+        2: "serpent",
+        3: "twofish",
+        4: "camellia",
+        5: "kuznyechik"
+    }
+    return switcher.get(input)
+
 def user_input():
-    if os.path.isdir("C:/Program Files/VeraCrypt") and os.path.isfile("C:/Program Files/VeraCrypt/VeraCrypt Format.exe"):       
-        print("VeraCrypt is installed!") 
+    if check_VC_integrity() == False:
+        print("VeraCrypt is not installed in your system")       
+    else:
         folder = input("Enter the folder to encrypt: ")
         folderPath = Path(folder)
         os.chdir(folderPath)
         cmd_foldername = os.path.basename(folderPath)
         cmd_size = os.path
         cmd_volumepath = folder+os.sep+cmd_foldername+".hc"
-        cmd_encryption = input()
-        cmd_hash = input()
-        cmd_fileSystem = input()
+        print_encryption_menu()
+        option = input()
+        encryption = choose_encryption(option)
+        cmd_encryption = repr(encryption)
+
+        #cmd_hash = input()
+        #cmd_fileSystem = input()
         aux_size = get_folder_size(folder) 
         size = (1.25 * aux_size)
         if size >= 1024:
             cmd_size = "10M"
         else:
             cmd_size = repr(math.ceil(size/1024))+"K"
+        cmd_size = "10M"
 
         print(cmd_size)
         base = "C:"+os.sep
@@ -42,9 +69,8 @@ def user_input():
         pathObject = Path(newPath)
         VCpath = pathObject.parent.absolute()
         os.chdir(VCpath)
-        subprocess.call(["VeraCrypt Format.exe","/create", cmd_volumepath,"/password", "test", "/hash", "sha512", "/encryption", "serpent", "/filesystem", "FAT", "/size", cmd_size, "/force"])
+        subprocess.call(["VeraCrypt Format.exe","/create", cmd_volumepath,"/password", "test", "/hash", "sha512", "/encryption", cmd_encryption, "/filesystem", "FAT", "/size", cmd_size, "/force"])
     # "C:\Program Files\VeraCrypt\VeraCrypt Format.exe" /create c:\Data\test.hc "/password test /hash sha512 /encryption serpent" /filesystem FAT /size 10M /force
-    else:
-        print("Veracrypt not in system")
+
 
 user_input()
