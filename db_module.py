e11e76fa297df3c8ea08628b42f1816ab9e437f4
AlberLC <alberl13@ucm.es>
2022-02-24 17:22:51 +0100

    Dropbox search implemented

--- db_module.py
+++ db_module.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from importlib.metadata import metadata
 import webbrowser
 import dropbox
 from dropbox import auth
@@ -8,7 +9,7 @@ import os
 class Db_object:
     def __init__(self):
         self.set_up()
-        self.list_folders()
+        self.search_folder("SER")
         return
 
     def set_up(self):
@@ -28,9 +29,45 @@ class Db_object:
         print("Successfully set up client!")
         return
     
-    def list_folders(self):
-        folder_list = self.dbx.files_list_folder('')
+    def list_folders(self,folder):
+        folder_list = self.dbx.files_list_folder("/"+folder)
         for entry in folder_list.entries:
             print(entry.name)
         return
+
+    def search_folder(self,folder):
+        found = self.dbx.files_search('',"/"+folder)
+        folder_list = []
+        if len(found.matches) == 0:
+            print("No matches found")
+            return -1
+        else:
+            if len(found.matches) > 1:
+                for entry in found.matches:
+                    if isinstance(entry.metadata,dropbox.files.FolderMetadata):
+                        folder_list.append(entry)
+                        
+                if len(folder_list) == 0:
+                    print("No matching folders found!")
+                    return -1
+                else:
+                    print("Select a folder to encrypt:")
+                    index = 1
+                    for entry in folder_list:
+                        meta = entry.metadata
+                        print(repr(index) + ". "+ meta.path_display)
+                        index = index + 1 
+                    selection = input()
+                    selection_num = int(selection)
+                    selection_num = selection_num-1
+                    return folder_list[selection_num]
+
+            else:
+                single_entry = found.get(0)
+                if single_entry and isinstance(single_entry.metadata,dropbox.files.FolderMetadata):
+                    print("Folder Found!")
+                    return single_entry
+                else:
+                    print("No matching folders found!")
+                    return -1
 
