3d27f542ae1c70b8d42af3aeed1d5e2c46a18ce4
AlberLC <alberl13@ucm.es>
2022-03-03 23:33:49 +0100

    Precondition Analysis on encryption

--- main.py
+++ main.py
@@ -86,6 +86,7 @@ class Main:
     ###################################################################################################
     ###################################################################################################
     
+    #P -> folder = pathlike
     def encrypt(self, folder):
         if folder == NULL:
             self.folderDict = self.fs.input_folder_encrypt()
@@ -94,20 +95,36 @@ class Main:
         self.user_input_encrypt()
         self.password_input()
         print("Encrypting base volume...")
-        if self.vc.VC_Encryption(self.folderDict["volume_path"], self.permuted_password, self.cmd_hash, self.cmd_encryption, self.cmd_fs, self.volume_size, self.folderDict["folder_path"]) == -1:
-            return
+        #P -> volume_path does not exist, X/:: not mounted
+        if (os.path.isfile(self.folderDict["volume_path"]) == False) or (os.path.isdir("X:"+os.sep) == False):
+            if self.vc.VC_Encryption(self.folderDict["volume_path"], self.permuted_password, self.cmd_hash, self.cmd_encryption, self.cmd_fs, self.volume_size, self.folderDict["folder_path"]) == -1:
+                return
+            else:
+                print("First layer of encryption successfully created!")
+            print("Splitting and permutating the volume...")
         else:
-            print("First layer of encryption successfully created!")
-        print("Splitting and permutating the volume...")
-
-        if  self.fd.split_file(self.folderDict["volume_path"], self.folderDict["folder_name"]) == -1: 
-            print("Could not split encrypted file: Not enough space on device for performing the operation")
-            self.vc.VC_Decryption(self.folderDict["volume_path"],self.permuted_password, self.folderDict["folder_path"])
-            return
+            print("Could not perform encryption")
+            if os.path.isfile(self.folderDict["volume_path"]):
+                print("File: "+ self.folderDict["volume_path"]+ " already exists!")
+                return
+            else:
+                print("Virtual drive 'X' is already being used!")
+                return
+        #P -> volume exists
+        if os.path.isfile(self.folderDict["volume_path"]):
+            if  self.fd.split_file(self.folderDict["volume_path"], self.folderDict["folder_name"]) == -1: 
+                print("Could not split encrypted file: Not enough space on device for performing the operation")
+                self.vc.VC_Decryption(self.folderDict["volume_path"],self.permuted_password, self.folderDict["folder_path"])
+                return
+            else:
+                print("Encrypted file succesfully splitted")
         else:
-            print("Encrypted file succesfully splitted")
+            print("Encrypted container could not be created, nothing to split!")
+            return
+        #P -> none
         self.fd.populateDict(self.pw.get_alpha(),self.pw.get_beta(),len(self.permuted_password),self.permuted_password)
         print("Encrypting milestone files...")
+        #P -> none
         if self.fd.intermediate_encryption() == -1:
             self.fd.intermediate_decryption(self.folderDict["folder_parent"], self.folderDict["folder_name"])
             self.fd.restore_file(self.folderDict["folder_name"])
@@ -140,6 +157,7 @@ class Main:
     ###################################################################################################
     ###################################################################################################
 
+    #P -> folder = pathlike w/ no extension
     def decrypt(self, folder):
         if folder == NULL:
             self.folderDict = self.fs.input_folder_decrypt()
