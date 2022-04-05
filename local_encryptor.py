e726d84c3f3796348b3a3be4db6f770a2b2d9251
AlberLC <alberl13@ucm.es>
2022-04-05 19:27:14 +0200

    Deeper refactor and testing

--- local_encryptor.py
+++ local_encryptor.py
@@ -13,29 +13,42 @@ class Local_encryptor(Encryptor):
             self.folderDict = self.fs.input_folder_encrypt()
         else:
             self.folderDict = self.fs.create_dict(folder)
-        backup = self.fs.directory_backup_create(self.folderDict['folder_path'])
+        self.backup = self.fs.directory_backup_create(self.folderDict['folder_path'])
         self.ctr.user_input_encrypt(self.folderDict)
         self.ctr.password_input()
         print("Encrypting base volume...")
         #P -> volume_path does not exist, X/:: not mounted
+        self.deep_layer_encryption()
+        print("First layer of encryption successfully created!")
+        print("Splitting and permutating the volume...")
+        self.milestone_encryption()
+        print("Aggregating files...")
+        self.outer_layer_encryption()
+        print("Encryption complete!")
+        print("Good luck!")
+        shutil.rmtree(self.backup)
+        return self.folderDict
+
+
+
+
+    def deep_layer_encryption(self):
         if (os.path.isfile(self.folderDict["volume_path"]) == False) or (os.path.isdir("X:"+os.sep) == False):
             if self.vc.VC_Encryption(self.folderDict["volume_path"], self.ctr.permuted_password, self.ctr.cmd_hash, self.ctr.cmd_encryption, self.ctr.cmd_fs, self.ctr.volume_size, self.folderDict["folder_path"]) == -1:
                 if os.path.isfile(self.folderDict["volume_path"]):
                     os.remove(self.folderDict["volume_path"])
                 shutil.rmtree(self.folderDict['folder_path'])
-                self.fs.directory_backup_rename(backup,self.folderDict['folder_path'])
+                self.fs.directory_backup_rename(self.backup,self.folderDict['folder_path'])
                 return
             else:
-                print("First layer of encryption successfully created!")
-            print("Splitting and permutating the volume...")
-        else:
-            print("Could not perform encryption")
-            if os.path.isfile(self.folderDict["volume_path"]):
-                print("File: "+ self.folderDict["volume_path"]+ " already exists!")
-            else:
-                print("Virtual drive 'X' is already being used!")
+                print("Could not perform encryption")
+                if os.path.isfile(self.folderDict["volume_path"]):
+                    print("File: "+ self.folderDict["volume_path"]+ " already exists!")
+                else:
+                    print("Virtual drive 'X' is already being used!")
             return
-        #P -> volume exists
+
+    def milestone_encryption(self):
         if os.path.isfile(self.folderDict["volume_path"]):
             if  self.fd.split_file(self.folderDict["volume_path"], self.folderDict["folder_name"]) == -1: 
                 for i in range(self.fd.file_number):
@@ -44,7 +57,7 @@ class Local_encryptor(Encryptor):
                         os.remove(chunk_file_name)
                 if os.path.isfile(self.folderDict["volume_path"]):
                     os.remove(self.folderDict["volume_path"])
-                self.fs.directory_backup_rename(backup,self.folderDict['folder_path'])
+                self.fs.directory_backup_rename(self.backup,self.folderDict['folder_path'])
                 return
             else:
                 print("Encrypted file succesfully splitted")
@@ -65,12 +78,13 @@ class Local_encryptor(Encryptor):
                     os.remove(chunk_file_name_enc)
             if os.path.isfile(self.folderDict["volume_path"]):
                 os.remove(self.folderDict["volume_path"])
-            self.fs.directory_backup_rename(backup,self.folderDict['folder_path'])
+            self.fs.directory_backup_rename(self.backup,self.folderDict['folder_path'])
             return
         else:
             print("Milestone files successfully encrypted!")
-
-        print("Aggregating files...")
+            return
+    
+    def outer_layer_encryption(self):
         if self.fs.folder_aggregation(self.folderDict["folder_parent"], self.folderDict["folder_name"], self.fd.file_number) == -1:
             for i in range(self.fd.file_number):
                 chunk_file_name = self.folderDict['volume_path']+"_"+repr(i)+".bin"
@@ -82,7 +96,7 @@ class Local_encryptor(Encryptor):
             if os.path.isfile(self.folderDict["volume_path"]):
                 os.remove(self.folderDict["volume_path"])
             shutil.rmtree(self.folderDict['folder_path'])
-            self.fs.directory_backup_rename(backup,self.folderDict['folder_path'])
+            self.fs.directory_backup_rename(self.backup,self.folderDict['folder_path'])
             return
         print("Encrypting last layer...")
         self.final_pass = self.pw.password_permutation(self.ctr.permuted_password)
@@ -91,14 +105,8 @@ class Local_encryptor(Encryptor):
             if os.path.isfile(self.folderDict["volume_path"]):
                 os.remove(self.folderDict["volume_path"])
             shutil.rmtree(self.folderDict['folder_path'])
-            self.fs.directory_backup_rename(backup,self.folderDict['folder_path'])
+            self.fs.directory_backup_rename(self.backup,self.folderDict['folder_path'])
             return
-        print("Encryption complete!")
-        print("Good luck!")
-        shutil.rmtree(backup)
-        return self.folderDict
-
-     
 
 
     #P -> folder = pathlike w/ no extension
@@ -108,17 +116,38 @@ class Local_encryptor(Encryptor):
         else:
             self.folderDict = self.fs.create_dict(folder)
         self.ctr.password_input()
+        self.decryption_init()
+        self.outer_layer_decryption()
+        print("Outer layer successfully decrypted!")
+        print("Fetching milestone file parameters...")
+        self.milestone_decryption()
+        print("Restoring file...")
+        self.fd.restore_file(self.folderDict["folder_name"])
+        print("Originial file successfully restored!")
+        print("Decrypting deep layer...")
+        self.deep_layer_decryption()
+        os.remove(self.backup)
+        print("Decryption Complete!")
+        print("Stay safe!")
+        return self.folderDict
+
+
+    def decryption_init(self):
         print("Preparing decryption environment...")
         self.final_pass = self.pw.password_permutation(self.ctr.permuted_password)
         print("Decrypting outer layer...")
         os.chdir(self.folderDict["folder_parent"])
         base_vol = os.path.basename(self.folderDict["volume_path"])
-        vol_path = self.folderDict["folder_parent"].__str__() + os.sep + base_vol
-        self.backup = self.fs.file_backup_creation(vol_path)
+        self.vol_path = self.folderDict["folder_parent"].__str__() + os.sep + base_vol
+        self.backup = self.fs.file_backup_creation(self.vol_path)
         if self.backup == -1:
-            return
-        
-        if self.vc.VC_Decryption(vol_path,self.final_pass, self.folderDict["folder_path"]) == -1:
+            return -1
+        else:
+            return 0
+    
+
+    def outer_layer_decryption(self):
+         if self.vc.VC_Decryption(self.vol_path,self.final_pass, self.folderDict["folder_path"]) == -1:
             if os.path.isdir("X:"+os.sep):
                 os.chdir(self.VCpath)
                 subprocess.call(["C:\Program Files\VeraCrypt\VeraCrypt.exe", "/dismount", "X", "/quit", "/silent", "/force"])
@@ -131,13 +160,13 @@ class Local_encryptor(Encryptor):
                 os.chdir(self.folderDict["folder_parent"])
                 os.chmod(self.folderDict["folder_path"], 0o777)
                 shutil.rmtree(self.folderDict["folder_path"])
-            if os.path.isfile(vol_path):
-                os.remove(vol_path)
-            self.fs.backup_rename(self.backup, vol_path)
+            if os.path.isfile(self.vol_path):
+                os.remove(self.vol_path)
+            self.fs.backup_rename(self.backup, self.vol_path)
             return
-        print("Outer layer successfully decrypted!")
-        print("Fetching milestone file parameters...")
-        self.fd.file_number = self.fs.retake_file_number(self.fs.remove_file_extension(vol_path))
+    
+    def milestone_decryption(self):
+        self.fd.file_number = self.fs.retake_file_number(self.fs.remove_file_extension(self.vol_path))
         self.fd.populateDict(self.ctr.alpha_base,self.ctr.beta_base, len(self.ctr.permuted_password),self.ctr.permuted_password)
         print("Parameters fetched!")
         if self.fs.folder_decompossition(self.folderDict["folder_parent"], self.folderDict["folder_name"], self.fd.file_number) == -1:
@@ -149,7 +178,7 @@ class Local_encryptor(Encryptor):
                     os.remove(chunk_file_name)
             os.chdir(self.folderDict["folder_parent"])
             shutil.rmtree(self.folderDict["folder_path"])
-            self.fs.backup_rename(self.backup, vol_path)
+            self.fs.backup_rename(self.backup, self.vol_path)
             return
         print("Decrypting milestone files...")
         if self.fd.intermediate_decryption(self.folderDict["folder_parent"], self.folderDict["folder_name"]):
@@ -161,15 +190,13 @@ class Local_encryptor(Encryptor):
                     os.remove(chunk_file_name_1)
                 if os.path.isfile(chunk_file_name_2):
                     os.remove(chunk_file_name_2)
-            self.fs.backup_rename(self.backup, vol_path)
+            self.fs.backup_rename(self.backup, self.vol_path)
             print("Could not finish intermediate decryption. Exiting...")
             return
         print("Milestone files successfully decrypted!")
-        print("Restoring file...")
-        self.fd.restore_file(self.folderDict["folder_name"])
-        print("Originial file successfully restored!")
-        print("Decrypting deep layer...")
-        if self.vc.VC_Decryption(vol_path,self.ctr.permuted_password, self.folderDict["folder_path"]) == -1:
+    
+    def deep_layer_decryption(self):
+        if self.vc.VC_Decryption(self.vol_path,self.ctr.permuted_password, self.folderDict["folder_path"]) == -1:
             if os.path.isdir("X:"+os.sep):
                 os.chdir(self.VCpath)
                 subprocess.call(["C:\Program Files\VeraCrypt\VeraCrypt.exe", "/dismount", "X", "/quit", "/silent", "/force"])
@@ -182,11 +209,8 @@ class Local_encryptor(Encryptor):
                 os.chdir(self.folderDict["folder_parent"])
                 os.chmod(self.folderDict["folder_path"], 0o777)
                 shutil.rmtree(self.folderDict["folder_path"])
-            if os.path.isfile(vol_path):
-                os.remove(vol_path)
-            self.fs.backup_rename(self.backup, vol_path)
+            if os.path.isfile(self.vol_path):
+                os.remove(self.vol_path)
+            self.fs.backup_rename(self.backup, self.vol_path)
             return
-        os.remove(self.backup)
-        print("Decryption Complete!")
-        print("Stay safe!")
-        return self.folderDict
+        
