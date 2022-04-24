c6e6d87204d00acf9d5916c02b09209cad73d0dc
AlberLC <alberl13@ucm.es>
2022-04-24 18:08:35 +0200

    Button generalization

--- GUI.py
+++ GUI.py
@@ -29,13 +29,9 @@ class DS_interface:
         self.load_home_images()
         logo_lbl = Label(frame,image=self.logo,bg="#232137")
         logo_lbl.pack()
-        local_btn = self.local_button_config(frame)
-        drive_btn = self.drive_button_config(frame)
-        dropbox_btn = self.dropbox_botton_config(frame)
-        testframe = Frame(self.root,background="#232137")
-        local_btn.configure(command=(lambda:self.move_forward(frame,testframe)))
-        drive_btn.configure(command=(lambda:self.move_forward(frame,testframe)))
-        dropbox_btn.configure(command=(lambda:self.move_forward(frame,testframe)))
+        local_btn = self.button_config(frame,self.local,self.local_hover)
+        drive_btn = self.button_config(frame,self.drive,self.drive_hover)
+        dropbox_btn = self.button_config(frame, self.dropbox, self.dropbox_hover)
         return frame
 
     
@@ -47,8 +43,8 @@ class DS_interface:
         logo_lbl = Label(frame,image=self.logo,bg="#232137")
         logo_lbl.pack()
         self.load_local_images()
-        self.scatter_button_config(frame)
-        self.centralized_button_config(frame)
+        scatter = self.button_config(frame, self.scatter, self.scatter_hover)
+        centralized = self.button_config(frame, self.centralized, self.centralized_hover)
         return frame
 
 
@@ -68,82 +64,22 @@ class DS_interface:
         self.centralized= PhotoImage(file='GUI/centralized.png')
         self.centralized_hover= PhotoImage(file='GUI/centralized_act.png')
     
-    def local_button_config(self,frame):
-        local_btn = Button(frame,image=self.local,bg="#232137",borderwidth=0,activebackground="#232137")
-        local_btn.pack()
+    def button_config(self,frame,def_img,act):
+        btn = Button(frame,image=def_img,bg="#232137",borderwidth=0,activebackground="#232137")
+        btn.pack()
 
 
         def on_enter_local(event):
-            local_btn.config(image=self.local_hover)
+            btn.config(image=act)
         
         
         def on_leave_local(event):
-            local_btn.config(image=self.local)
+            btn.config(image=def_img)
         
-        local_btn.bind("<Enter>",on_enter_local)
-        local_btn.bind("<Leave>",on_leave_local)
-        return local_btn
+        btn.bind("<Enter>",on_enter_local)
+        btn.bind("<Leave>",on_leave_local)
+        return btn
 
-    def scatter_button_config(self,frame):
-        scatter_btn = Button(frame,image=self.scatter,bg="#232137",borderwidth=0,activebackground="#232137")
-        scatter_btn.pack()
-
-
-        def on_enter_local(event):
-            scatter_btn.config(image=self.scatter_hover)
-        
-        
-        def on_leave_local(event):
-            scatter_btn.config(image=self.scatter)
-        
-        scatter_btn.bind("<Enter>",on_enter_local)
-        scatter_btn.bind("<Leave>",on_leave_local)
-        return scatter_btn
-
-    def centralized_button_config(self,frame):
-        centralized_btn = Button(frame,image=self.centralized,bg="#232137",borderwidth=0,activebackground="#232137")
-        centralized_btn.pack()
-
-
-        def on_enter_local(event):
-            centralized_btn.config(image=self.centralized_hover)
-        
-        
-        def on_leave_local(event):
-            centralized_btn.config(image=self.centralized)
-        
-        centralized_btn.bind("<Enter>",on_enter_local)
-        centralized_btn.bind("<Leave>",on_leave_local)
-        return centralized_btn
-
-
-    def drive_button_config(self,root):
-        drive_btn = Button(root,image=self.drive,bg="#232137",borderwidth=0,activebackground="#232137")
-        drive_btn.pack()
-
-        def on_enter_drive(event):
-            drive_btn.config(image=self.drive_hover)
-        
-        def on_leave_drive(event):
-            drive_btn.config(image=self.drive)
-        
-        drive_btn.bind("<Enter>",on_enter_drive)
-        drive_btn.bind("<Leave>",on_leave_drive)
-        return drive_btn
-    
-    def dropbox_botton_config(self,root):
-        dropbox_btn = Button(root,image=self.dropbox,bg="#232137",borderwidth=0, activebackground="#232137")
-        dropbox_btn.pack()
-
-        def on_enter_dropbox(event):
-            dropbox_btn.config(image=self.dropbox_hover)
-        
-        def on_leave_dropbox(event):
-            dropbox_btn.config(image=self.dropbox)
-        
-        dropbox_btn.bind("<Enter>",on_enter_dropbox)
-        dropbox_btn.bind("<Leave>",on_leave_dropbox)
-        return dropbox_btn
 
     def move_forward(self, old_frame, new_frame):
         old_frame.pack_forget()
