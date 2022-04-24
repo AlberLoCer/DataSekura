6c05af776b1efa779b637d4b839d1e2f2dbaaa0d
AlberLC <alberl13@ucm.es>
2022-04-24 18:30:37 +0200

    Positioning improvements

--- GUI.py
+++ GUI.py
@@ -13,7 +13,7 @@ class DS_interface:
         self.root= Tk()
         self.root.title("DataSekura")
         self.root.resizable(False,False)
-        self.root.geometry("600x500")
+        self.root.geometry("700x500")
         self.frame_dict['home'] = self.home_screen()
         self.frame_dict['local'] = self.local_screen()
         def on_closing():
@@ -28,10 +28,10 @@ class DS_interface:
         frame = Frame(self.root,background="#232137")
         self.load_home_images()
         logo_lbl = Label(frame,image=self.logo,bg="#232137")
-        logo_lbl.pack()
-        local_btn = self.button_config(frame,self.local,self.local_hover)
-        drive_btn = self.button_config(frame,self.drive,self.drive_hover)
-        dropbox_btn = self.button_config(frame, self.dropbox, self.dropbox_hover)
+        logo_lbl.grid(row=0,column=0,pady=20)
+        local_btn = self.button_config(frame,self.local,self.local_hover,1)
+        drive_btn = self.button_config(frame,self.drive,self.drive_hover,2)
+        dropbox_btn = self.button_config(frame, self.dropbox, self.dropbox_hover,3)
         return frame
 
     
@@ -42,9 +42,11 @@ class DS_interface:
         frame.pack(fill="both",expand=True)
         logo_lbl = Label(frame,image=self.logo,bg="#232137")
         logo_lbl.pack()
+        buttons_frame = Frame(frame,background="#232137")
+        buttons_frame.pack()
         self.load_local_images()
-        scatter = self.button_config(frame, self.scatter, self.scatter_hover)
-        centralized = self.button_config(frame, self.centralized, self.centralized_hover)
+        scatter = self.button_config(buttons_frame, self.scatter, self.scatter_hover,1)
+        centralized = self.button_config(buttons_frame, self.centralized, self.centralized_hover,2)
         return frame
 
 
@@ -64,10 +66,10 @@ class DS_interface:
         self.centralized= PhotoImage(file='GUI/centralized.png')
         self.centralized_hover= PhotoImage(file='GUI/centralized_act.png')
     
-    def button_config(self,frame,def_img,act):
+    def button_config(self,frame,def_img,act,idx):
         btn = Button(frame,image=def_img,bg="#232137",borderwidth=0,activebackground="#232137")
-        btn.pack()
-
+        btn.grid(column=0,row=idx,pady=20)
+        frame.grid_columnconfigure(0,weight=1)
 
         def on_enter_local(event):
             btn.config(image=act)
