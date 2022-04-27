e985f57de32bc1fc8d93b06a950cf49f42340d66
AlberLC <alberl13@ucm.es>
2022-04-27 11:15:41 +0200

    Config screen ready

--- GUI.py
+++ GUI.py
@@ -13,6 +13,7 @@ class DS_interface:
         self.frame_dict = dict()
         # Create an instance of tkinter frame
         self.root= Tk()
+        self.load_images()
         self.root.title("DataSekura")
         self.root.resizable(False,False)
         self.root.geometry("700x500")
@@ -23,23 +24,33 @@ class DS_interface:
         self.frame_dict["enc_dec_scatter"] = self.enc_dec_screen()
         self.frame_dict["enc_dec_drive"] = self.enc_dec_screen()
         self.frame_dict["enc_dec_dropbox"] = self.enc_dec_screen()
+
+        self.frame_dict["config"] = self.config_screen()
         home = self.frame_dict["home"]
         local = self.frame_dict["local"]
+
         enc_dec_local = self.frame_dict["enc_dec_local"]
         enc_dec_scatter = self.frame_dict["enc_dec_scatter"]
         enc_dec_drive = self.frame_dict["enc_dec_drive"]
         enc_dec_dropbox = self.frame_dict["enc_dec_dropbox"]
+
+        config_local = self.frame_dict["config"]
+
         home["frame"].pack(fill="both",expand=True)
         home["local"].configure(command=lambda:(self.switch_screen(home["frame"],local["frame"])))
         home["drive"].configure(command=lambda:(self.switch_screen(home["frame"],enc_dec_drive["frame"])))
         home["dropbox"].configure(command=lambda:(self.switch_screen(home["frame"],enc_dec_dropbox["frame"])))
+        
         local["back"].configure(command=lambda:(self.switch_screen(local["frame"],home["frame"])))
         local["scatter"].configure(command=lambda:(self.switch_screen(local["frame"],enc_dec_scatter["frame"])))
         local["centralized"].configure(command=lambda:(self.switch_screen(local["frame"],enc_dec_local["frame"])))
+        
+        enc_dec_local["encrypt"].configure(command=lambda:(self.switch_screen(enc_dec_local["frame"],config_local["frame"])))        
         enc_dec_local["back"].configure(command=lambda:(self.switch_screen(enc_dec_local["frame"],local["frame"])))
         enc_dec_scatter["back"].configure(command=lambda:(self.switch_screen(enc_dec_scatter["frame"],local["frame"])))
         enc_dec_drive["back"].configure(command=lambda:(self.switch_screen(enc_dec_drive["frame"],home["frame"])))
         enc_dec_dropbox["back"].configure(command=lambda:(self.switch_screen(enc_dec_dropbox["frame"],home["frame"])))
+        config_local["back"].configure(command=lambda:(self.switch_screen(config_local["frame"],enc_dec_local["frame"])))
         
         def on_closing():
             if messagebox.askokcancel("Quit", "Do you want to exit DataSekura?"):
@@ -52,7 +63,6 @@ class DS_interface:
     def home_screen(self):
         screen = dict()
         frame = Frame(self.root,background="#232137")
-        self.load_home_images()
         logo_lbl = Label(frame,image=self.logo,bg="#232137")
         logo_lbl.grid(row=0,column=0,pady=20)
         local_btn = self.button_config(frame,self.local,self.local_hover,1,0)
@@ -71,7 +81,6 @@ class DS_interface:
         logo_lbl.pack()
         buttons_frame = Frame(frame,background="#232137")
         buttons_frame.pack()
-        self.load_local_images()
         scatter = self.button_config(buttons_frame, self.scatter, self.scatter_hover,1,10)
         centralized = self.button_config(buttons_frame, self.centralized, self.centralized_hover,2,10)
         back = self.button_config(buttons_frame,self.goBack, self.goBack_hover,3,10)
@@ -88,20 +97,33 @@ class DS_interface:
         logo_lbl.pack()
         buttons_frame = Frame(frame,background="#232137")
         buttons_frame.pack()
-        self.load_enc_dec_images()
         encrypt = self.button_config(buttons_frame, self.encrypt, self.encrypt_hover,1,10)
         decrypt = self.button_config(buttons_frame, self.decrypt, self.decrypt_hover,2,10)
         back = self.button_config(buttons_frame,self.goBack, self.goBack_hover,3,10)
         screen["frame"] = frame
-        screen["ecnrypt"] = encrypt
+        screen["encrypt"] = encrypt
         screen["decrypt"] = decrypt
+        screen["back"] = back
+        return screen
+    
+    def config_screen(self):
+        screen = dict()
+        frame = Frame(self.root,background="#232137")
+        logo_lbl = Label(frame,image=self.logo,bg="#232137")
+        logo_lbl.pack()
+        buttons_frame = Frame(frame,background="#232137")
+        buttons_frame.pack()
+        auto = self.button_config(buttons_frame, self.auto, self.auto_hover,1,10)
+        manual = self.button_config(buttons_frame, self.manual, self.manual_hover,2,10)
+        back = self.button_config(buttons_frame,self.goBack, self.goBack_hover,3,10)
+        screen["frame"] = frame
+        screen["auto"] = auto
+        screen["manual"] = manual
         screen["back"] = back
         return screen
 
 
-
-
-    def load_home_images(self):
+    def load_images(self):
         self.back= PhotoImage(file='GUI/ds_back.png')
         self.logo= PhotoImage(file='GUI/logo.png')
         self.local= PhotoImage(file='GUI/local.png')
@@ -110,20 +132,20 @@ class DS_interface:
         self.drive_hover= PhotoImage(file='GUI/drive_act.png')
         self.dropbox= PhotoImage(file='GUI/dropbox_std.png')
         self.dropbox_hover= PhotoImage(file='GUI/dropbox_act.png')
-    
-    def load_local_images(self):
         self.scatter= PhotoImage(file='GUI/scatter.png')
         self.scatter_hover= PhotoImage(file='GUI/scatter_act.png')
         self.centralized= PhotoImage(file='GUI/centralized.png')
         self.centralized_hover= PhotoImage(file='GUI/centralized_act.png')
         self.goBack = PhotoImage(file='GUI/back.png')
         self.goBack_hover = PhotoImage(file='GUI/back_act.png')
-    
-    def load_enc_dec_images(self):
         self.encrypt= PhotoImage(file='GUI/encrypt.png')
         self.encrypt_hover= PhotoImage(file='GUI/encrypt_act.png')
         self.decrypt = PhotoImage(file='GUI/decrypt.png')
         self.decrypt_hover = PhotoImage(file='GUI/decrypt_act.png')
+        self.auto= PhotoImage(file='GUI/auto.png')
+        self.auto_hover= PhotoImage(file='GUI/auto_act.png')
+        self.manual = PhotoImage(file='GUI/manual.png')
+        self.manual_hover = PhotoImage(file='GUI/manual_act.png')
     
     def button_config(self,frame,def_img,act,idx,padding):
         btn = Button(frame,image=def_img,bg="#232137",borderwidth=0,activebackground="#232137")
